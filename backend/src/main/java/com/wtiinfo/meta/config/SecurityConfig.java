4e8e80b17f412a5ddf5827466cc928fb11e0df4f
Wando Borges <wanbrwan@gmail.com>
2022-07-18 21:10:23 -0300

    Corrigindo classe

--- backend/src/main/java/com/wtiinfo/meta/config/SecurityConfig.java
+++ backend/src/main/java/com/wtiinfo/meta/config/SecurityConfig.java
@@ -18,11 +18,12 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
 		http.headers().frameOptions().disable();
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+
 		return http.build();
 	}
 
